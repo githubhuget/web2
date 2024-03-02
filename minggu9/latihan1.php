872907449756576b6e55b27193f8345f90dacc32
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2024-03-02 17:55:00 +0700

    latihan get

--- minggu9/latihan1.php
+++ minggu9/latihan1.php
@@ -1,13 +1,40 @@
 <?php
-// lingkup variabel
-$angka = 10;
+$mahasiswa = [
+    [
+    "nama" => "Teguh Firmansyah",
+    "NIM" => "23510031",
+    "Jurusan" => "Sistem Informasi",
+    "email" => "teguh@stimata.ac.id",
+    "gambar" => "teguh.jpeg"
+    ],
 
-function Angka ()
-{
-    $angka = 20;
-    echo $angka;
-}
+    [
+    "nama" => "Firman Saputra",
+    "NIM" => "23510032",
+    "Jurusan" => "Sistem Informasi",
+    "email" => "firman@stimata.ac.id",
+    "gambar" => "firman.jpeg"
+    ]
+];
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan GET</title>
+</head>
+<body>
+<h1>Daftar Mahasiswa</h1>
+    <ul>
+        <?php foreach ($mahasiswa as $mhs) :?>
+        <li>
+            <a
+                 href="latihan1.php?nama=<?= $mhs["nama"]; ?>&NIM=  <?= $mhs["NIM"]; ?>&
+                 jurusan=<?= $mhs["jurusan"]; ?>&email=<?= $mhs["email"]; ?>&gambar=<?= 
+                 $mhs["gambar"]; ?>"><?= $mhs["nama"];   ?></a>
 
-echo $angka;
-echo "<br>";
-Angka();
+        </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html> 
---
+++ minggu9/latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deteil Mahasiswa</title>
+</head>
+
+<body>
+    <ul>
+        <li><img src="img/<?= $_GET["gambar"]; ?>"></li>
+        <li><?= $_GET["nama"]; ?></li>
+        <li><?= $_GET["nim"]; ?></li>
+        <li><?= $_GET["email"]; ?></li>
+        <li><?= $_GET["jurusan"]; ?></li>
+    </ul>
+
+    <a href="latihan1.php">kembali ke Halaman sebelumnya</a>
+</body>
+</html>
